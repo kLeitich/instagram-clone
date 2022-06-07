3321133bcf320fc1801f5eb3a042364c2da05184
kleitich <kevleitich@gmail.com>
2022-06-07 12:14:40 +0300

    adding auth to the repo

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def welcome(request):
     return render(request,'index.html')
+
+def register_user(request):
+    return render(request,'auth/register.html')
