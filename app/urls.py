ae4b8446740f545f42850ba4c6b88748cdb8a7bf
kleitich <kevleitich@gmail.com>
2022-06-09 21:09:49 +0300

    adding log and register working

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+urlpatterns=[
+    path('',views.welcome,name = 'welcome'),
+    path('register',views.register_user,name = 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/login.html'), name='logout'),
+    # path('profile',views.profile,name='profile'),
+    # path('update_profile/<int:id>',views.update_profile,name='update_profile')
+]   
--- app/views.py
+++ app/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from .forms import UserRegistrationForm,UpdateUserProfileForm
+from .models import Profile,User
 
 # Create your views here.
+def welcome(request):
+    return render(request,'index.html')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, f'Your account has been created. You can log in now!')    
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+
+    context = {'form': form}
+    return render(request, 'auth/register.html', context)
+
+# def profile(request):
+#     current_user = request.user
+#     user = User.objects.get(id = current_user.id)
+#     profile=Profile.filter_profile_by_id(user.id) 
+#     print(profile)
+#     return render(request,'profile.html',{'profile':profile})
+
+
+# def update_profile(request,id):
+#     user = User.objects.get(id=id)
+#     profile = Profile.objects.get(user = user)
+#     if request.method == "POST":
+#             form = UpdateUserProfileForm(request.POST,request.FILES,instance=profile)
+#             if form.is_valid():
+#                 form.save()
+#                 return redirect('profile') 
+#             else:
+#                 return render(request,'update_profile.html',{'form':form})
+#     else:        
+#         form = UpdateUserProfileForm(instance=profile)
+#     return render(request, 'update_profile.html', {'form':form})
