97d521355d1654792741997bb98fdcc9a1c0d920
kleitich <kevleitich@gmail.com>
2022-06-10 02:59:47 +0300

    adding test to the repo

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from .models import Profile, Image
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestImage(TestCase):
+    def setUp(self):
+        self.profile_test = Profile(name='joy', user=User(username='joy'))
+        self.profile_test.save()
+
+        self.image_test = Image(image='pic.png', name='test', caption='default test', user=self.profile_test)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image_test, Image))
+
+    def test_save_image(self):
+        self.image_test.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
+
+    def test_delete_image(self):
+        self.image_test.delete_image()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) < 1)
+
+    def test_total_likes(self):
+        self.image_test.save_image()
+        self.image_test.likes.add(self.profile_test)
+        self.assertTrue(self.image_test.total_likes() == 1)
+
+class TestProfile(TestCase):
+    def setUp(self):
+        self.user = User(username='kleitch')
+        self.user.save()
+        self.profile_test = Profile(id=1, profile_picture='pic.jpg', bio='bio',user=self.user)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile_test, Profile))
+
+    def test_save_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) > 0)
+
+    def test_delete_profile(self):
+        self.profile_test.save_profile()
+        self.profile_test.delete_profile()
+        after = Profile.objects.all()
+        self.assertTrue(len(after) == 0)
+
+    def test_search_profile(self):
+        self.profile_test.save_profile()
+        after = Profile.search_profile('kleitich')
+        self.assertTrue(len(after) > 0)
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
@@ -7,6 +8,7 @@ from .forms import UploadImageModelForm, UserRegistrationForm,UpdateUserProfileF
 from .models import Profile,User,Image,Follow
 
 # Create your views here.
+@login_required(login_url='login')  
 def welcome(request):
     current_user = request.user
     posts = Image.objects.all()
@@ -24,10 +26,10 @@ def welcome(request):
         }
 
     return render(request,'index.html',context)
-
+@login_required(login_url='login')
 def dm(request):
     return render(request,'dm.html')
-
+@login_required(login_url='login')
 def image_upload(request):
     current_user = request.user
     if request.method == 'POST':
@@ -39,10 +41,10 @@ def image_upload(request):
     else:
         form = UploadImageModelForm()
     return render(request,'image_upload.html',{'form':form})
-
+@login_required(login_url='login')
 def explore(request):
     return render(request,'explore.html')
-
+@login_required(login_url='login')
 def notification(request):
     return render(request,'notification.html')
 
@@ -59,7 +61,7 @@ def register_user(request):
 
     context = {'form': form}
     return render(request, 'auth/register.html', context)
-
+@login_required(login_url='login')
 def profile(request):
     current_user = request.user
     user = User.objects.get(id = current_user.id)
@@ -67,7 +69,7 @@ def profile(request):
     posts = Image.objects.filter(user = user.id)
     return render(request,'profile.html',{'profile':profile,'posts':posts})
 
-
+@login_required(login_url='login')
 def update_profile(request,id):
     user = User.objects.get(id=id)
     profile = Profile.objects.get(user = user)
@@ -81,21 +83,21 @@ def update_profile(request,id):
     else:        
         form = UpdateUserProfileForm(instance=profile)
     return render(request, 'update_profile.html', {'form':form})
-
+@login_required(login_url='login')
 def follow(request,id):
     if request.method == 'GET':
         user_follow=User.objects.get(pk=id)
         follow_user=Follow(follower=request.user, followed=user_follow)
         follow_user.save()
         return redirect('user_profile' ,username=user_follow.username)
-    
+@login_required(login_url='login')    
 def unfollow(request,id):
     if request.method=='GET':
         user_unfollow=User.objects.get(pk=id)
         unfollow_user=Follow.objects.filter(follower=request.user,followed=user_unfollow)
         unfollow_user.delete()
         return redirect('user_profile' ,username=user_unfollow.username)
-
+@login_required(login_url='login')
 def user_profile(request, username):
     user_prof = get_object_or_404(User, username=username)
     if request.user == user_prof:
